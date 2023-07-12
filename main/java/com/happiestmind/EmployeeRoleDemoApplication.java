77701a1af2e31460afb5af5bfd84383ae0db2fb2
Wasim.akram2 <infotowasim@gmail.com>
2023-07-12 13:12:38 +0530

    first commit

--- main/java/com/happiestmind/EmployeeRoleDemoApplication.java
+++ main/java/com/happiestmind/EmployeeRoleDemoApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class EployeeRoleDemoApplication {
+public class EmployeeRoleDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EployeeRoleDemoApplication.class, args);
+		SpringApplication.run(EmployeeRoleDemoApplication.class, args);
 	}
 
 	@Bean
